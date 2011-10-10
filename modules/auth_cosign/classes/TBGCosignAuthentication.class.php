8231f293c2e489b3b794558d192e755f8d348bdc
Samuel Leathers <sbl5007@chrysostom.local>
2011-10-10 16:18:37 -0400

    Add logout and autoLogin functions to auth_cosign

--- modules/auth_cosign/classes/TBGCosignAuthentication.class.php
+++ modules/auth_cosign/classes/TBGCosignAuthentication.class.php
@@ -194,5 +194,27 @@
         public function isOutdated() {
             return false;
         }
+		/*
+		 * Actions on logout
+		 */
+		public function logout()
+		{
+			
+		}
+		
+		/*
+		 * Actions on login - if there are no credentials supplied try an autologin
+		 * Not applicable for this module
+		 * 
+		 * Return:
+		 * true - succeeded operation but no autologin
+		 * false - invalid cookies found
+		 * Row from TBGUsersTable - succeeded operation, user found
+		 * 
+		 */
+		public function autoLogin()
+		{
+			return true;
+		}
 	}
 
